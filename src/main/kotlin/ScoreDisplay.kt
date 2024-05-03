57fbf4342afc418a3bda3dbdb95a3f55f5be1dec
DenisOrlovv <A48250@alunos.isel.pt>
2024-05-03 09:28:34 +0100

    Finish of SetScore and off command from ScoreDisplay module.

--- src/main/kotlin/ScoreDisplay.kt
+++ src/main/kotlin/ScoreDisplay.kt
@@ -9,11 +9,17 @@ object ScoreDisplay {
     fun setScore(value: Int) {
         var numberToString = value.toString().map{ it.toString().toInt() }
         numberToString = numberToString.reversed()
+
+        var currentNumberToString = currentValue.toString().map{ it.toString().toInt() }
+        currentNumberToString = currentNumberToString.reversed()
+
+
         for (i in 0..numberToString.size){
-            var temp = i.shl(4)
-            temp += numberToString[i]
-            SerialEmitter.send(SerialEmitter.Destination.SCORE, temp,7)
-            println(temp)
+            if (numberToString[i] != currentNumberToString[i]) {
+                var temp = i.shl(4)
+                temp += numberToString[i]
+                SerialEmitter.send(SerialEmitter.Destination.SCORE, temp, 7)
+            }
         }
 
 
@@ -22,6 +28,8 @@ object ScoreDisplay {
     }
     // Envia comando para desativar/ativar a visualização do mostrador de pontuação
     fun off(value: Boolean){
-
+        val command = 7
+        if (value) SerialEmitter.send(SerialEmitter.Destination.SCORE, command.shl(4) + 1, 7 )
+        else SerialEmitter.send(SerialEmitter.Destination.SCORE, command.shl(4) + 0, 7 )
     }
 }
